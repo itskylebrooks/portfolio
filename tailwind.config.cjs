54052ecdb92dac7bb6ddfd69b84025d44f8121fe
Kyle Brooks <consist-canteen8e@icloud.com>
2025-10-07 03:54:38 +0200

    feat: add IBM Plex Sans font and update Tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "'IBM Plex Sans'",
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Color Emoji"',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          'sans-serif'
+        ]
+      }
+    }
+  },
   plugins: [require("@tailwindcss/typography")],
 };
